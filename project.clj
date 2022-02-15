6f88d42e23c74c48383d08ece619ea37000ee83b
Ivan Grishaev <ivan@grishaev.me>
2022-02-15 17:31:29 +0300

    add clojars

--- project.clj
+++ project.clj
@@ -6,6 +6,9 @@
   :url
   "https://github.com/igrishaev/pact"
 
+  :deploy-repositories
+  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
+
   :license
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url "https://www.eclipse.org/legal/epl-2.0/"}
